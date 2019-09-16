b8876a39664cda4f7515edd501a14748e9880aed
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-09-16 14:56:53 +0700

    Android Bundle - BundleActivity

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 
 public class ProfileBundleActivity extends AppCompatActivity {
 
+    public static final String USERNAME_KEY = "username";
+    public static final String NAME_KEY = "name";
+    public static final String AGE_KEY = "age";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
+
 
         // TODO: bind here
 
